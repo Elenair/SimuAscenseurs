a0dfd8c8f97cb5db00813710ea054494297aa6fd
hadoop <hadoop@hadoop-ENLE69KB>
2015-11-16 09:42:35 +0100

    génération du code java à partir de papyrus dans le modèle ui

---
+++ Tp-Application-Java-Equipe2/src/univavignon/m1ilsen/aa/sea/ui/ConfigureFlow.java
@@ -0,0 +1,5 @@
+package univavignon.m1ilsen.aa.sea.ui;
+
+public class ConfigureFlow { 
+
+}
---
+++ Tp-Application-Java-Equipe2/src/univavignon/m1ilsen/aa/sea/ui/ElevatorFile.java
@@ -0,0 +1,35 @@
+package univavignon.m1ilsen.aa.sea.ui;
+
+import java.io.File;
+
+public class ElevatorFile {
+
+	/**
+	 * 
+	 */
+	public File file;
+
+	/**
+	 * Getter of file
+	 */
+	public File getFile() {
+	 	 return file; 
+	}
+
+	/**
+	 * Setter of file
+	 */
+	public void setFile(File file) { 
+		 this.file = file; 
+	}
+
+	/**
+	 * 
+	 * @return 
+	 */
+	public boolean getElevatorFile() { 
+		// TODO Auto-generated method
+		return false;
+	 } 
+
+}
