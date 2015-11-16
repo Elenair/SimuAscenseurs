39f4822c158fb81774fde2a3899bdc3128929294
uapv1300104 <uapv1300104@ge-st5-102.univ-avignon.fr>
2015-11-16 10:41:52 +0100

    Ajout des classes du paquage elevator

---
+++ Tp-Application-Java-Equipe2/src/univavignon/m1ilsen/aa/sea/elevator/imp/Sensor.java
@@ -0,0 +1,44 @@
+package univavignon.m1ilsen.aa.sea.elevator.imp;
+
+public class Sensor {
+
+	/**
+	 * 
+	 */
+	public boolean cross ;
+	/**
+	 * 
+	 */
+	public int id_sensor;
+	/**
+	 * Getter of cross 
+	 */
+	public boolean getCross () {
+	 	 return cross ; 
+	}
+	/**
+	 * Setter of cross 
+	 */
+	public void setCross (boolean cross ) { 
+		 this.cross  = cross ; 
+	}
+	/**
+	 * Getter of id_sensor
+	 */
+	public int getId_sensor() {
+	 	 return id_sensor; 
+	}
+	/**
+	 * Setter of id_sensor
+	 */
+	public void setId_sensor(int id_sensor) { 
+		 this.id_sensor = id_sensor; 
+	}
+	/**
+	 * 
+	 */
+	public void detection () { 
+		// TODO Auto-generated method
+	 } 
+
+}
