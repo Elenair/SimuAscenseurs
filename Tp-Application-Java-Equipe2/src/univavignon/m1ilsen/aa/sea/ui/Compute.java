b496312252b09fa639984da17187a70218e7d891
uapv1100085 <uapv1100085@ge-st2-115.univ-avignon.fr>
2015-11-25 17:09:09 +0100

    calculs

--- Tp-Application-Java-Equipe2/src/univavignon/m1ilsen/aa/sea/ui/Compute.java
+++ Tp-Application-Java-Equipe2/src/univavignon/m1ilsen/aa/sea/ui/Compute.java
@@ -1,43 +1,60 @@
 package univavignon.m1ilsen.aa.sea.ui;
 
+import java.util.Map;
+import java.util.Vector;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
 public class Compute {
 
 	/**
 	 * 
 	 * @return 
 	 */
-	public long TripTime( long [] table ) { 
+	
+	Map simulation_map; // results
+	
+	public void set_simulation_map( Map map ){
+		this.simulation_map = map;
+			}
+	
+	
+	
+	// temps d'un voyage 
+	
+	public Vector <Long> TripTime( ) { 
 		// TODO Auto-generated method
-		long trip_time = table[3] -table[2];
-		return 0 ;
-	 }
-
-	/**
-	 * 
-	 * @return 
-	 */
-	public float WaitingTimeAverage() { 
-		// TODO Auto-generated method
+		Vector<Long> hd = (Vector) this.simulation_map.get("heure_depart");
+		Vector<Long> ha = (Vector) this.simulation_map.get("heure_arrivee");		
 		
-		return 0;
-	 }
-
-	/**
-	 * 
-	 * @return 
-	 */
-	public float TripTimeStandartDeviation() { 
-		// TODO Auto-generated method
-		return 0;
-	 }
-
-	/**
-	 * 
-	 * @return 
-	 */
-	public float WaitingTimeStandartDeviation() { 
-		// TODO Auto-generated method
-		return 0;
-	 } 
+		int size = hd.size();
+		Vector result = new Vector <Long> ();
+		
+		for( int i=0 ; i < size ; i++ ){
+			long diff = (Long)ha.get(i)-hd.get(i) ;
+			result.add( i , diff ); 
+		}
+		
+		return result;
+	}
+	
+	
+	// temps moyen des voyages 
+	
+		public Long AverageTripTime( ) { 
+			// TODO Auto-generated method	
+			Vector result = new Vector <Long> ();
+			result = TripTime();
+			long somme = 0;
+			for( int i=0 ; i < result.size() ; i++ ){
+				somme = (Long)result.get(i) + somme ;
+							}
+			return somme/result.size();
+		}
+	
+		
+	// la durée maximale d'un voyage
+		
+		
 
 }
