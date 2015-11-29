d677a7ece144c196544a5b627c25180c5f093279
mabmray <mabmray@yahoo.fr>
2015-11-29 18:11:22 +0100

    c bon shaft

--- Tp-Application-Java-Equipe2/src/univavignon/m1ilsen/aa/sea/elevator/imp/ShaftCage.java
+++ Tp-Application-Java-Equipe2/src/univavignon/m1ilsen/aa/sea/elevator/imp/ShaftCage.java
@@ -4,26 +4,45 @@ import univavignon.m1ilsen.aa.sea.commontypes.Direction;
 import univavignon.m1ilsen.aa.sea.commontypes.ElevatorState;
 import univavignon.m1ilsen.aa.sea.controlsystem.interface_.IElevatorNotifier;
 import univavignon.m1ilsen.aa.sea.elevator.interface_.IElevatorCommand;
+import univavignon.m1ilsen.aa.sea.sequencer.interface_.Event;
 
-public class ShaftCage implements IElevatorCommand {
+public class ShaftCage implements Event,IElevatorCommand {
 
 	private IElevatorNotifier iElevatorNotifier;
 	
-	double heightShaftCage;
-	double heightBetween2Level;
-	Sensor[] tabSensors;
-	Engine engine;
-	Elevator elevator;
-	@Override
-	public void stopAtLevel() {
+	private double heightShaftCage;
+	private double heightBetween2Level;
+	private Engine engine;
+	private Elevator elevator;
+	private int Nblevel;
+	private long lasttrigger;
+	private Direction direction;
+	private double movingheight;
+	private Sensor sensor;
+	
+	 public ShaftCage(double h0,double h1,int Nblevel,double speed, long closetime, long opentime, long waitime)
+	 {
+		 this.heightShaftCage = h0;
+		 this.heightBetween2Level = h1;
+		 this.Nblevel = Nblevel;
+		 this.engine = new Engine(speed);
+		 this.elevator = new Elevator(closetime,opentime,waitime);
+		 this.movingheight = 0.0;
+		 }
+	
+	public void stopAtLevel() 
+	{
 		// TODO Auto-generated method stub
+		
+		this.elevator.state = ElevatorState.stopping;
 		
 	}
 
 	@Override
 	public void move(Direction direction) {
 		// TODO Auto-generated method stub
-		
+		this.direction = direction;
+		this.elevator.state = ElevatorState.moving;
 	}
 	
 	/**
@@ -34,4 +53,47 @@ public class ShaftCage implements IElevatorCommand {
 	}
 
 
+	@Override
+	public void trigger(long t) {
+		
+		// TODO Auto-generated method stub
+		int level = 0 ;long tbst;
+		tbst = t - lasttrigger;
+		lasttrigger = t;
+		
+		if(this.elevator.state == ElevatorState.moving){
+			if(this.elevator.state == ElevatorState.closed){
+			engine.runningEng(direction);
+		    	if(direction == direction.up){
+				
+			    	movingheight += this.engine.getSpeed() * tbst;
+			    	level = (int)(movingheight /heightBetween2Level);
+				
+		    	}else if(direction == direction.down){
+				
+				    movingheight -= this.engine.getSpeed() * tbst;
+				    level = (int)(movingheight /heightBetween2Level);
+			}
+			
+			if(movingheight % heightBetween2Level == 0){
+				sensor.detection();
+				iElevatorNotifier.notifyLevel(level, lasttrigger);
+			}
+				sensor.sensorNoCross();
+		   }
+		}
+		
+		else if(this.elevator.state == ElevatorState.stopping){
+			engine.offEng();
+			if(this.elevator.state == ElevatorState.closed){
+				elevator.Openning(lasttrigger);
+			}else if (this.elevator.state == ElevatorState.open){
+				elevator.waiting(lasttrigger);
+			}else if(this.elevator.state == ElevatorState.waiting){
+				elevator.closeDoor(lasttrigger);
+			}
+			iElevatorNotifier.notifyState(this.elevator.state, lasttrigger);
+		}
+			
+	}
 }
